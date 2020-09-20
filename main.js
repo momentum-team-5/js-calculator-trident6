39c3fa3f30c89285c6230529c8a817e3426a4cf9
trident6 <philip.johnson.it@gmail.com>
2020-09-20 17:08:10 -0400

    getting closer

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 let button = document.querySelector('.main-container')
+let input = ''
+let output = ''
+
 button.addEventListener('click', function(event) {
-    console.log('CLICKED!!!')
-    console.log('button', event.target.id, 'clicked')
     let messageDiv = document.querySelector('.message')
     let messagePara = document.createElement('p')
+    target = event.target.id
 
-    // let text = document.createTextNode('Hello There!')
-
-    messagePara.innerText = event.target.id
-        // messagePara.innerText = 'Hello There!'
+    if (target === '=') {
+        let messageDiv = document.getElementById('message')
+        let messageParaOld = messageDiv.getElementsByTagName('p')[0]
+        messageDiv.removeChild(messageParaOld)
+        output = eval(input)
+        console.log('equal')
+        console.log(output)
+    }
+    if (target === 'C') {
+        let messageDiv = document.getElementById('message')
+        let messageParaOld = messageDiv.getElementsByTagName('p')[0]
+        messageDiv.removeChild(messageParaOld)
+        output = ''
+        input = ''
+        console.log(input)
+        console.log('clear')
+    }
+    if ((target !== 'C') && (target !== '=')) {
+        let messageDiv = document.getElementById('message')
+        let messageParaOld = messageDiv.getElementsByTagName('p')[0]
+        messageDiv.removeChild(messageParaOld)
+        input += target
+        output = input
+        console.log('if3')
+        console.log(input)
+    }
+    messagePara.innerText = output
     messageDiv.appendChild(messagePara)
+
 })
